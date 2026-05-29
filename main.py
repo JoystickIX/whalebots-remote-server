c2951e4a49172aa13708aab8788a8525022e3b79
JoystickIX <mostafa.1006.231@gmail.com>
2026-05-29 22:58:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -33,10 +33,10 @@ OWNER_IDS = {316613385485680650, 641191095258185728}
 # AUTO UPDATE
 # =====================================
 
-LATEST_VERSION = "1.0.3"
+LATEST_VERSION = "1.0.4"
 
 EXE_DOWNLOAD_LINK = (
-    "https://github.com/JoystickIX/whalebots-remote-server/releases/download/V1.0.3/WhaleBotsRemote.exe"
+    "https://github.com/JoystickIX/whalebots-remote-server/releases/download/V1.0.4/WhaleBotsRemote.exe"
 )
 
 # =====================================
